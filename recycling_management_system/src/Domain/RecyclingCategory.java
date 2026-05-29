01820cb918418c78acd3a8cd9640e71b843d067c
raluca-t <raluca_tabacaru@icloud.com>
2026-05-29 20:03:44 +0200

    done commenting RecyclingCategory ENUM

--- recycling_management_system/src/Domain/RecyclingCategory.java
+++ recycling_management_system/src/Domain/RecyclingCategory.java
@@ -1,4 +1,17 @@
 package Domain;
+
+
+
+/**
+ * Domain layer
+ * 
+ * Represents different recycling categories in the system.
+ * 
+ * A recycling category is used to:
+ * - classify materials
+ * - support recycling guidance generation
+ * - organize materials in the system
+ */
 public enum RecyclingCategory {
     PLASTIC,
     METAL,
